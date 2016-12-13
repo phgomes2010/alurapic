6b074d7b820005ed7e221de90528b1161158861f
PH <phgomes2010@gmail.com>
2016-12-13 01:01:29 -0200

    Finalização da aula 1

---
+++ public/js/controllers/fotos-controller.js
@@ -0,0 +1,6 @@
+angular.module('alurapic').controller('FotosController', function($scope){
+    $scope.foto = {
+        titulo : 'Luffy Gear 4',
+        url : 'sources/luffy-gear-4.jpg'
+    };
+});
